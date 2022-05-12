b173d3a16bab56fe12331a63f49fe5d1722f7de3
Let-nathan <letournel.nathan@gmail.com>
2022-05-12 16:40:16 +0200

    startAPI

--- src/Api/Client.php
+++ src/Api/Client.php
@@ -6,30 +6,23 @@ use Symfony\Contracts\HttpClient\HttpClientInterface;
 class Client
 {
     private HttpClientInterface $client;
+
     public function __construct()
     {
         $this->client = HttpClient::create([
             'verify_peer' => false,
-            'base_uri' => 'https://koumoul.com/data-fair/api/v1/datasets/agribalyse-synthese/',
+            'base_uri' => 'https://api.websitecarbon.com/',
         ]);
     }
-    public function getFruitsByName(string $name): array
+
+    public function getSite(string $name): array
     {
-        $response = $this->client->request('GET', 'lines', [
+        $response = $this->client->request('GET', 'site', [
             'query' => [
-                'select' => 'Code_AGB,Nom_du_Produit_en_Français', //
-                'q' => $name,
+                'url' => $name,
             ]
         ]);
         return $response->toArray();
     }
-    public function getValues(string $field): array
-    {
-        $response = $this->client->request('GET', 'values/'.$field, [
-            'query' => [
-                'size' => 200,
-            ]
-        ]);
-        return $response->toArray();
-    }
+
 }
--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -8,7 +8,22 @@ class HomeController extends AbstractController
 {
     public function index(): string
     {
+        $errors = [];
         $api = new Client();
-        //print_r($api->getFruitsByName('Agneau'));die;
-        print_r($api->getValues('Nom_du_Produit_en_Français'));
+        if(isset($_GET['url'])) {
+            $response = $api->getSite($_GET['url']);
+//            foreach ($response as $index => $value) {
+//
+//            }
+        } else {
+            $error['url'] = 'input something';
+        }
+
+        var_dump($_GET);
+        var_dump($response);
+        return $this->twig->render('Home/index.html.twig', [
+            'errors' => $errors,
+            'response' => $response
+        ]);
     }
+}
